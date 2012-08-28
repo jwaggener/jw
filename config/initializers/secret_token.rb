307fcab18fc289061c941cc6e8f127dd411f73c9
John Waggener <jwaggener@vitrue.com>
2012-08-28 12:05:33 -0400

    an initial add of files to the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JwBlue::Application.config.secret_token = '4308ef552788097575ddb4d15569c21fc40a71a1deaaad9376ac008dd800d5873464dc4b92119b1093dc6d555db4e8f7e084785853a47a431ab36cda28eee66b'
